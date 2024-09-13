3d1cfad85732a9e75523ae8416b490769decaf5a
oussama ait moummad <ousamatessla@gmail.com>
2024-09-13 17:25:19 +0100

    edit the ui

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <h1>Create Your Own Tasks</h1>
 <p>Create Tasks website Using ReactJs</p>
-<h3><b>Link : </b>https://osmcode05.github.io/creating_tasks_react_app</h3>
+<h3><b>Link : </b>https://osmcode05.github.io/create-tasks</h3>
 <img src='public/preview.jpg' />
--- src/Components/ReadTask.js
+++ src/Components/ReadTask.js
@@ -3,26 +3,14 @@ import { FaPencilAlt, FaTrashAlt } from "react-icons/fa";
 export default function ReadTask({ tasks, handleEditTask, handleDeleteTask }) {
   return (
     <div className="container">
-      {tasks.length === 0 ? (
-        <h1>No Tasks</h1>
-      ) : (
-        tasks.map((task, index) => (
-          <div key={index} className="task">
-            <p>{task}</p>
-            <div>
-              <FaPencilAlt
-                role="button"
-                onClick={() => handleEditTask(index)}
-              />
-              <FaTrashAlt
-                style={{ marginLeft: "1.2rem" }}
-                role="button"
-                onClick={() => handleDeleteTask(index)}
-              />
-            </div>
-          </div>
-        ))
-      )}
+      {tasks.length <= 0 && <h1>No Tasks</h1>}
+      {tasks.map((task, index) => (
+        <div key={index} className="task">
+          <p>{task}</p>
+          <FaPencilAlt size={22} onClick={() => handleEditTask(index)} />
+          <FaTrashAlt size={20} onClick={() => handleDeleteTask(index)} />
+        </div>
+      ))}
     </div>
   );
 }
